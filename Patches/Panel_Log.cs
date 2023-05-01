6b44df2a6f6acc8d76a0b7e4a44634753900f6b8
VonDoogles <1239309+VonDoogles@users.noreply.github.com>
2023-04-30 20:14:00 -0700

    Auto-select the region the player is currently in when opening the surveyed locations UI. Auto-focus the region or location list when the mouse enters the associated panel. (This allows MouseWheel to scroll the list where the cursor is.)

--- Patches/Panel_Log.cs
+++ Patches/Panel_Log.cs
@@ -41,6 +41,86 @@ namespace QualityOfLife
                         }
                     }
                 }
+
+                if ( __instance.m_CartographySectionObject.activeSelf )
+                {
+                    Vector3 MouseLoc = Input.mousePosition;
+
+                    var PanelContains = delegate ( UIPanel Panel, Vector3 MouseLoc )
+                    {
+                        Vector3 Min = Panel.anchorCamera.WorldToScreenPoint( Panel.worldCorners[ 0 ] );
+                        Vector3 Max = Min;
+
+                        foreach ( Vector3 Corner in Panel.worldCorners )
+                        {
+                            Vector3 ScreenCorner = Panel.anchorCamera.WorldToScreenPoint( Corner );
+                            Min.x = Math.Min( Min.x, ScreenCorner.x );
+                            Min.y = Math.Min( Min.y, ScreenCorner.y );
+                            Max.x = Math.Max( Max.x, ScreenCorner.x );
+                            Max.y = Math.Max( Max.y, ScreenCorner.y );
+                        }
+
+                        Rect PanelRect = Rect.MinMaxRect( Min.x, Min.y, Max.x, Max.y );
+                        return PanelRect.Contains( MouseLoc );
+                    };
+
+                    UIPanel RegionListPanel = __instance.m_SurveyListRegionScrollbar.transform.parent.GetComponent<UIPanel>();
+                    UIPanel SurveyListPanel = __instance.m_SurveyListScrollbar.transform.parent.GetComponent<UIPanel>();
+
+                    if ( SurveyListPanel != null && PanelContains( SurveyListPanel, MouseLoc ) )
+                    {
+                        if ( __instance.m_SurveyState != PanelLogSurveyStates.AchievementList )
+                        {
+                            __instance.m_SurveyState = PanelLogSurveyStates.AchievementList;
+                        }
+                    }
+                    else if ( RegionListPanel != null && PanelContains( RegionListPanel, MouseLoc ) )
+                    {
+                        if ( __instance.m_SurveyState != PanelLogSurveyStates.RegionList )
+                        {
+                            __instance.m_SurveyState = PanelLogSurveyStates.RegionList;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch( typeof( Panel_Log ), "UpdateCartographyPage" )]
+    internal class Patch_Panel_Log_UpdateCartographyPage
+    {
+        static void Postfix( Panel_Log __instance )
+        {
+            if ( Settings.Instance.UIExtraControls )
+            {
+                string SceneName = GameManager.m_LastOutdoorSceneSet.name;
+
+                int RegionIndex = -1;
+                for ( int Index = 0; Index < __instance.m_SurveyRegionList.Count; ++Index )
+                {
+                    SurveyRegionInfo RegionInfo = __instance.m_SurveyRegionList[ Index ];
+                    if ( RegionInfo.m_SceneName == SceneName )
+                    {
+                        RegionIndex = Index;
+                        break;
+                    }
+                }
+
+                int ClickedIndex = -1;
+                for ( int Index = 0; Index < __instance.m_SurveyRegionDisplayList.Count; ++Index )
+                {
+                    SurveyRegionListItem RegionItem = __instance.m_SurveyRegionDisplayList[ Index ];
+                    if ( RegionItem != null && RegionItem.m_Index == RegionIndex )
+                    {
+                        ClickedIndex = Index;
+                        break;
+                    }
+                }
+
+                if ( ClickedIndex != -1 )
+                {
+                    __instance.OnSurveyRegionClicked( ClickedIndex );
+                }
             }
         }
     }
