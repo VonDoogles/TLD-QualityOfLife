9f3d4a7608cc99478550be3d542360520560b0d4
VonDoogles <1239309+VonDoogles@users.noreply.github.com>
2024-06-08 01:23:58 -0700

    Fixed a bug where swapping equipment wouldn't update clothing condition bars.

---
+++ Patches/ClothingSlot.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using UnityEngine;
+
+namespace QualityOfLife
+{
+	[HarmonyPatch( typeof( ClothingSlot ), "UpdateSlotInfo" )]
+    internal class Patch_ClothingSlot_UpdateSlotInfo
+    {
+		static void Postfix( ClothingSlot __instance )
+		{
+			UIProgressBar? ConditionFill = __instance.transform.FindChild( "ConditionFill" )?.GetComponent<UIProgressBar>();
+			if ( ConditionFill == null && Settings.Instance.ClothingConditionBars )
+			{
+				ConditionFill = WidgetUtils.MakeProgressBar( "ConditionFill", __instance.transform, 60 );
+			}
+
+			if ( ConditionFill != null )
+			{
+				ConditionFill.transform.localPosition = new Vector3( 8, -30, 0 );
+				ConditionFill.mFG.transform.localPosition = new Vector3( -38, 0, 0 );
+
+				if ( __instance.m_GearItem != null && Settings.Instance.ClothingConditionBars )
+				{
+					Color FillColor = __instance.m_GearItem.GetColorBasedOnCondition();
+					FillColor.a = 0.65f;
+					ConditionFill.mFG.color = FillColor;
+					ConditionFill.value = __instance.m_GearItem.GetNormalizedCondition();
+					WidgetUtils.SetActive( ConditionFill, true );
+				}
+				else
+				{
+					WidgetUtils.SetActive( ConditionFill, false );
+				}
+			}
+		}
+    }
+}
